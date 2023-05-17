f0986821e6e9a35c39d99f849d1d90d2bc89fd86
Rayh <raykah.silva@gmail.com>
2023-05-16 22:24:52 -0300

    tarefa feita

--- app/src/main/java/com/example/diceroller/a3.kt
+++ app/src/main/java/com/example/diceroller/a3.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 import android.widget.Button
 
 class a3 : AppCompatActivity() {
@@ -13,6 +14,10 @@ class a3 : AppCompatActivity() {
         setContentView(R.layout.activity_a3)
 
         val button: Button = findViewById(R.id.button3)
+
+        val webView: WebView = findViewById(R.id.webview)
+        webView.loadUrl("https://github.com/SoImRayh")
+
 
         button  .setOnClickListener {
             val intent: Intent = Intent(context, a4::class.java)
--- app/src/main/java/com/example/diceroller/a4.kt
+++ app/src/main/java/com/example/diceroller/a4.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 import android.widget.Button
+
 
 class a4 : AppCompatActivity() {
     private val context: Context get() = this
@@ -13,6 +15,10 @@ class a4 : AppCompatActivity() {
         setContentView(R.layout.activity_a4)
 
         val button: Button = findViewById(R.id.button4)
+        val webview: WebView = findViewById(R.id.webview2)
+        webview.settings.javaScriptEnabled = true
+        webview.loadUrl("https://lavajatodopedro.com")
+
         button.setOnClickListener {
             val intent: Intent = Intent(context, a5::class.java)
             startActivity(intent)
